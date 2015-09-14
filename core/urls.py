1aeeffd2f482ec912e90db71454c32f8d9e95634
Pranav Jindal <pranavjindal@DN51sd2c.SUNet>
2015-09-14 00:33:29 -0700

    Displaying data working

--- core/urls.py
+++ core/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 import core.views as coreviews
 
 urlpatterns = patterns('',
- url(r'^$', coreviews.LandingView.as_view()),
+	url(r'^$', coreviews.LandingView.as_view()),
+	url(r'location/$', coreviews.LocationListView.as_view()),
+	url(r'location/(?P<pk>\d+)/detail$', coreviews.LocationDetailView.as_view(), name='location_detail'),
 )
